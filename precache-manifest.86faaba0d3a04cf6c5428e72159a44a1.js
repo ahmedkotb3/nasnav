9c36992c1ed44da10c72eb9a09a32294470ee2cd
ahmedkotb3 <ahmedmohamed.kotb@yahoo.com>
2019-03-09 01:47:11 +0200

    Updates

---
+++ precache-manifest.86faaba0d3a04cf6c5428e72159a44a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdaef6fb96f438ef5219a8ed2a196470",
+    "url": "/nasnavshopping/static/media/logofooter.cdaef6fb.png"
+  },
+  {
+    "revision": "a18df940c1e1290f16ed",
+    "url": "/nasnavshopping/static/css/main.53cc28f3.chunk.css"
+  },
+  {
+    "revision": "8a283ff588d5c56e2248",
+    "url": "/nasnavshopping/static/js/runtime~main.4abc30b3.js"
+  },
+  {
+    "revision": "67dd8ddf648a58098c7d",
+    "url": "/nasnavshopping/static/js/2.31bb10ab.chunk.js"
+  },
+  {
+    "revision": "150eb462678c4d20d5ae55697dcf0284",
+    "url": "/nasnavshopping/static/media/twoMobiles.150eb462.png"
+  },
+  {
+    "revision": "c22e29d9820561a46f2c45d1195146b6",
+    "url": "/nasnavshopping/static/media/twoMobiles2.c22e29d9.png"
+  },
+  {
+    "revision": "b4fe3f8934e7d04e5bdfc4f83662f1f4",
+    "url": "/nasnavshopping/static/media/pic3.b4fe3f89.png"
+  },
+  {
+    "revision": "a18df940c1e1290f16ed",
+    "url": "/nasnavshopping/static/js/main.3d420b2d.chunk.js"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/nasnavshopping/static/media/Proxima Nova Semibold.6a386899.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/nasnavshopping/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "1a55f6f6658c8531a12833a4f3eab37c",
+    "url": "/nasnavshopping/static/media/back1.1a55f6f6.jpg"
+  },
+  {
+    "revision": "f7e3c228e754491a98fe3eb00aebb934",
+    "url": "/nasnavshopping/static/media/wave2.f7e3c228.png"
+  },
+  {
+    "revision": "f35beca8187ee4ae66b09528e4ceab44",
+    "url": "/nasnavshopping/static/media/map.f35beca8.png"
+  },
+  {
+    "revision": "5ec28b4abd3827585cecf999c0b39ad5",
+    "url": "/nasnavshopping/index.html"
+  }
+];
